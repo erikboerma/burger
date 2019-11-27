9489b7f4b8a4f9687144dab8d2abfe47c4e103f2
ErikBoerma <eboerma@gmail.com>
2019-11-27 16:05:05 -0500

    created app, added burgers.js as public, having issues with syncing up... probalby due to a burger or burgers error somewhere.  time to debug.. yea

---
+++ assets/js/burgers.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#burger").val().trim(),
+            devoured: "0"
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
+    });
+    $(".devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var newStatus = $(this).data("devoured");
+        
+        var eaten = {
+            devoured: true
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: eaten
+        }).then(
+            function () {
+                console.log("changed status to", newStatus);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+            );
+      
+    });
+
+});
