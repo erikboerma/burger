d6451ad139c26f78cdfdab0307ad55a84c06875a
ErikBoerma <eboerma@gmail.com>
2020-01-02 15:46:32 -0500

    updates to pages, issue getting handlebars to show

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 
-var burgers = require("../models/burger.js");
+var burgers = require("../models/burgers");
 
 router.get("/", function (req, res) {
     burgers.all(function (data) {
@@ -16,10 +16,10 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    cat.create([
-        "name", "devoured"
+    burgers.create([
+        "burger_name", "devoured"
     ], [
-        req.body.name, req.body.devoured
+        req.body.burger_name, req.body.devoured
     ], function (result) {
 
         res.json({ id: result.insertId });
@@ -34,13 +34,12 @@ router.put("/api/burgers/:id", function (req, res) {
     burgers.update({
         devoured: req.body.devoured
     }, condition, function (result) {
-        if (result.changedRows == 0) {
+        if (result.changedRows === 0) {
 
             return res.status(404).end();
-        } else {
+        } 
             res.status(200).end();
-        }
-    });
+        });
 });
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("Impossible Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false);
