7608db29edbb78bb16f0c52ac1b7a33a6f5ca389
ErikBoerma <eboerma@gmail.com>
2020-01-03 15:16:46 -0500

    ux updates

--- public/js/burger.js
+++ public/js/burger.js
@@ -14,6 +14,7 @@ $(function () {
             data: newBurger
         }).then(
             function () {
+                console.log("new burger added");
                 location.reload();
             }
         );
